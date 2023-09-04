a0e1e01bacd7ad62a3c8fd27819fb56c71169c48
Josie <tinaweizq@outlook.com>
2023-09-03 20:21:35 -0700

    Update blog with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 //jshint esversion:6
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
+import express from "express";
+import ejs from "ejs";
+import _ from "lodash";
+import mongoose from "mongoose";
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,59 +11,73 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/postDB");
+// Create a schema
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title cannot be empty"]
+  },
+  content: String
+})
+// Create a model
+const Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+app.get("/", (req, res) => {
+  Post.find({})
+      .then((foundPosts) => {
+          res.render("home", {
+            startingContent: homeStartingContent,
+            posts: foundPosts
+          });
+      })
+      .catch((err) => console.log(err))
 });
 
-app.get("/about", function(req, res){
+app.get("/about", (req, res) => {
   res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", (req, res) => {
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", (req, res) => {
   res.render("compose");
+
 });
 
-app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+app.post("/compose", (req, res) => {
+  const postTitle = req.body.postTitle;
+  const postBody = req.body.postBody;
 
-  posts.push(post);
+  const newPost = new Post({
+    title: postTitle,
+    content: postBody
+  })
 
+  newPost.save();
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", (req, res) => {
+  const requestedId = req.params.postId;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
-
+  Post.findById(requestedId)
+      .then((foundPost) => {
+          res.render("post", {
+            title: foundPost.title,
+            content: foundPost.content
+          });
+      })
+      .catch((err) => console.log(err))
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
